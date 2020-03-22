68bd78b6dea22833dcc630afa4b3c8ba48aa1011
Yuri Rolandi <sd.rolandi@gmail.com>
2020-03-22 19:26:52 -0300

    Updates

--- js/chunk-2d21098f.9b5ebaf1.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21098f"],{b924:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},c=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"rooms"},[n("div",{staticClass:"section"},[n("h1",[t._v("QUARTOS")])])])}],a={name:"Rooms"},i=a,l=n("2877"),o=Object(l["a"])(i,s,c,!1,null,null,null);e["default"]=o.exports}}]);
-//# sourceMappingURL=chunk-2d21098f.9b5ebaf1.js.map
