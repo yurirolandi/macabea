2cd4af9a1164810efc54e7af1b82d1fa94700011
Yuri Rolandi <sd.rolandi@gmail.com>
2020-03-23 21:59:36 -0300

    remove depend

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vueSmoothScroll from 'vue2-smooth-scroll'
-import AOS from 'aos';
 import Vuelidate from 'vuelidate'
 import BootstrapVue from 'bootstrap-vue'
 import scrollAnimation from './directives/scrollanimation';
@@ -12,7 +11,6 @@ Vue.directive('scrollanimation',  scrollAnimation )
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'aos/dist/aos.css';
 import './plugins/axios'
 import './plugins/fontawesome'
 
@@ -21,7 +19,6 @@ import './plugins/fontawesome'
 Vue.use(Vuelidate)
 Vue.use(BootstrapVue)
 Vue.use(vueSmoothScroll)
-AOS.init();
 
 Vue.config.productionTip = false
 
